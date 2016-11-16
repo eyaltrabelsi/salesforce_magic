1a14efd7e1d0f76e88f259985dffda542690702a
Eyal Trabelsi <etrabelsi@yotpo.com>
2016-11-16 17:57:28 +0200

    mend

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 from setuptools import setup
 
 here = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(here, 'README.rst')).read()
+README = open(os.path.join(here, 'README.md')).read()
 
 install_requires = [
     'ipython>=1.0',
@@ -12,14 +12,14 @@ install_requires = [
 ]
 
 setup(
-    name='ipython-salesforce',
-    packages=['ipython-salesforce'],
+    name='jupyter-salesforce',
+    packages=['src'],
     version='0.1',
     description='Access to salesforce via ipython',
     long_description=README,
     author='Eyal Trabelsi',
     author_email='eyaltrabelsi@gmail.com',
-    url='https://github.com/eyaltrabelsi/ipython-salesforce',
+    url='https://github.com/eyaltrabelsi/jupyter-salesforce',
     keywords=['sql', 'ipython', 'salesforce'],
     install_requires=install_requires
 )
